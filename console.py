50daaa45da3e1465cd255eaa8fdd49949bc2ca4a
Ignatius Kisekka <ignatiuskisekka@gmail.com>
2024-05-21 02:30:51 +0300

    6. Console 0.0.1

--- console.py
+++ console.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+
+"""Define console for AirBnB"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """HBNBCommand class"""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the program"""
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered"""
+        pass
+
+
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
